1ba2a5349f2f565f500fb292cf91878327177055
UglanovEA <uglanovea@gmail.com>
2021-05-31 17:03:31 +0300

    ver 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Technologies />
+
     </div>
   );
 }
-
+function Technologies() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          d
+        </li>
+        <li>
+          e
+        </li>
+        <li>
+          d
+        </li>
+      </ul>
+    </div>
+  );
+}
+function Header() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          a
+        </li>
+        <li>
+          b
+        </li>
+        <li>
+          c
+        </li>
+      </ul>
+    </div>
+  );
+}
 export default App;
