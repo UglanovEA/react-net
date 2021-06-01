95deb96053ff559e265b9669f926e4a1e3ffde58
UglanovEA <uglanovea@gmail.com>
2021-06-01 14:50:37 +0300

    ver 1.1

--- src/App.js
+++ src/App.js
@@ -2,44 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Technologies />
-
-    </div>
-  );
-}
-function Technologies() {
-  return (
-    <div className="App">
-      <ul>
-        <li>
-          d
-        </li>
-        <li>
-          e
-        </li>
-        <li>
-          d
-        </li>
-      </ul>
-    </div>
-  );
-}
-function Header() {
-  return (
-    <div className="App">
-      <ul>
-        <li>
-          a
-        </li>
-        <li>
-          b
-        </li>
-        <li>
-          c
-        </li>
-      </ul>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/American_Broadcasting_Company_Logo.svg/240px-American_Broadcasting_Company_Logo.svg.png' />
+      </header>
+      <nav className='nav'>
+        <ul>
+          <li><a href='#!'>Profile</a></li>
+          <li><a href='#!'>Messages</a></li>
+          <li><a href='#!'>News</a></li>
+          <li><a href='#!'>Music</a></li>
+          <li><a href='#!'>Settings</a></li>
+        </ul>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg'>
+          </img>
+        </div>
+      </div>
+      <div>
+        <img src=''></img>
+      </div>
     </div>
   );
 }
