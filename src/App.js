82b25fb8e13188c115444d763f787442201b3c5a
UglanovEA <uglanovea@gmail.com>
2021-06-03 16:29:09 +0300

    ver 1.5 add router

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Header from './componets/Header/Header.jsx';
 import Nav from './componets/Navigation/Navigation.jsx';
 import Profile from './componets/Profile/Profile.jsx';
 import Dialogs from './componets/Dialogs/Dialogs.jsx';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <div class='app-wrapper-content'>
-        {/* <Dialogs /> */}
-
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+        <div class='app-wrapper-content'>
+          <Route path='/Dialogs' component={Dialogs} />
+          <Route path='/Profile' component={Profile} />
+        </div>
       </div>
-
-      {/* <Profile /> */}
-
-    </div>
+    </BrowserRouter>
   );
 }
 export default App;
--- src/componets/Navigation/Navigation.jsx
+++ src/componets/Navigation/Navigation.jsx
@@ -3,8 +3,8 @@ function Nav() {
   return (
     <nav className={s.nav}>
       <ul>
-        <li><a href='#!'>Profile</a></li>
-        <li><a href='#!'>Messages</a></li>
+        <li><a href='/Profile'>Profile</a></li>
+        <li><a href='/Dialogs'>Messages</a></li>
         <li><a href='#!'>News</a></li>
         <li><a href='#!'>Music</a></li>
         <li><a href='#!'>Settings</a></li>
