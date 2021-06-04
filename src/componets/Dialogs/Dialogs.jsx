392f54853e3bbcd0e7f5ae6adb1c6f713264197b
UglanovEA <uglanovea@gmail.com>
2021-06-04 15:20:50 +0300

    ver 1.7 add dialogs

--- src/componets/Dialogs/Dialogs.jsx
+++ src/componets/Dialogs/Dialogs.jsx
@@ -1,7 +1,19 @@
 import s from './Dialogs.module.css';
 function Dialogs() {
   return (
-    <div>Dialogs</div>
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + " " + s.active}>Иван</div>
+        <div className={s.dialog}>Андрей</div>
+        <div className={s.dialog}>Саша</div>
+        <div className={s.dialog}>Сергей</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi</div>
+        <div className={s.message}>How are you</div>
+        <div className={s.message}>i am fine</div>
+      </div>
+    </div>
   )
 }
 
