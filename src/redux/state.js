ba8fc48f626d5bac870aa0c89beb2cdc627b8a0f
UglanovEA <uglanovea@gmail.com>
2021-06-15 14:09:31 +0300

    ver 2.9 add store

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,49 +1,50 @@
-let rerenderEntireTree = () => {
-
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, message: 'hi, how are you', likesCount: 12 },
-      { id: 2, message: 'it\'s my post', likesCount: 15 },
-    ],
-    newPostText: 'hello'
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+        { id: 1, message: 'hi, how are you', likesCount: 12 },
+        { id: 2, message: 'it\'s my post', likesCount: 15 },
+      ],
+      newPostText: 'hello'
+    },
+    dialogsPage: {
+      dialogs: [
+        { id: 1, name: 'Иван' },
+        { id: 2, name: 'Андрей' },
+        { id: 3, name: 'Сергей' },
+        { id: 4, name: 'Антон' },
+      ],
+      messages: [
+        { id: 1, message: "Hey" },
+        { id: 2, message: "How are you" },
+        { id: 3, message: "i am fine" },
+        { id: 4, message: "And you?" },
+      ]
+    }
   },
-  dialogsPage: {
-    dialogs: [
-      { id: 1, name: 'Иван' },
-      { id: 2, name: 'Андрей' },
-      { id: 3, name: 'Сергей' },
-      { id: 4, name: 'Антон' },
-    ],
-    messages: [
-      { id: 1, message: "Hey" },
-      { id: 2, message: "How are you" },
-      { id: 3, message: "i am fine" },
-      { id: 4, message: "And you?" },
-    ]
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.posts.push(newPost)
+    this._state.profilePage.newPostText = ""
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   }
 }
 
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.posts.push(newPost)
-  state.profilePage.newPostText = ""
-  rerenderEntireTree(state);
-}
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
+window.store = store;
