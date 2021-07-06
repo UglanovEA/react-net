ea9c3eb36a3d4cc6391607dfa6253270079be150
UglanovEA <uglanovea@gmail.com>
2021-07-06 14:30:00 +0300

    ver 4.5

--- src/componets/Users/UsersContainer.jsx
+++ src/componets/Users/UsersContainer.jsx
@@ -1,8 +1,39 @@
+import React from 'react';
+import * as axios from 'axios';
 import { connect } from 'react-redux';
-import { followActionCreator, setUserActionCreator, unfollowActionCreator } from '../../redux/users-reducer';
-import Users from './Users.jsx';
+import { followActionCreator, setCurrentPageActionCreator, setUserActionCreator, setUsersTotalCountActionCreator, unfollowActionCreator } from '../../redux/users-reducer';
+import Users from './Users.js';
 
 
+class UsersContainer extends React.Component {
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items)
+        this.props.setTotalUsersCount(response.data.totalCount)
+      })
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber)
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items)
+      })
+  }
+
+  render() {
+    return <Users
+      totalUsersCount={this.props.totalUsersCount}
+      pageSize={this.props.pageSize}
+      currentPage={this.props.currentPage}
+      onPageChanged={this.onPageChanged}
+      users={this.props.users}
+      follow={this.props.follow}
+      unfollow={this.props.unfollow}
+    />
+  };
+};
 let mapStateToProps = (state) => {
   return {
     users: state.usersPage.users,
@@ -22,6 +53,12 @@ let mapDispatchToProps = (dispatch) => {
     setUsers: (users) => {
       dispatch(setUserActionCreator(users))
     },
+    setCurrentPage: (pageNumber) => {
+      dispatch(setCurrentPageActionCreator(pageNumber))
+    },
+    setTotalUsersCount: (totalCount) => {
+      dispatch(setUsersTotalCountActionCreator({ totalCount }))
+    }
   }
 };
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
