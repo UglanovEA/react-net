b49a8e6e29cea05487a046d11f4ca4a88f6052c1
UglanovEA <uglanovea@gmail.com>
2021-07-16 16:44:12 +0300

    ver 4.9

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App(props) {
         <Route path='/Dialogs'
           render={() => <DialogsContainer />}
         />
-        <Route path='/Profile'
+        <Route path='/Profile/:userId?'
           render={() => <ProfileContainer />}
         />
         <Route path='/Users'
