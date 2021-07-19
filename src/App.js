8273dd730b1a7ada9552d36dce70586bb6283d68
UglanovEA <uglanovea@gmail.com>
2021-07-19 15:23:03 +0300

    ver 5.0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 import './App.css';
-import Header from './componets/Header/Header.jsx';
 import Nav from './componets/Navigation/Navigation.jsx';
 import { Route } from 'react-router-dom';
 import DialogsContainer from './componets/Dialogs/DialogsContainer';
 import UsersContainer from './componets/Users/UsersContainer.jsx';
 import ProfileContainer from './componets/Profile/ProfileContainer';
+import HeaderContainer from './componets/Header/HeaderContainer';
 
 function App(props) {
   return (
     <div className='app-wrapper'>
-      <Header />
+      <HeaderContainer />
       <Nav />
       <div className='app-wrapper-content'>
         <Route path='/Dialogs'
--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -1,8 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import s from './Header.module.css';
-function Header() {
+
+function Header(props) {
   return (
     <header className={s.header}>
       <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/American_Broadcasting_Company_Logo.svg/240px-American_Broadcasting_Company_Logo.svg.png' alt="logo" />
+      <div className={s.loginBlock}>
+        {props.isAuth ? props.login :
+          <NavLink to={'/login'}>Login</NavLink>}
+      </div>
     </header>
   )
 }
